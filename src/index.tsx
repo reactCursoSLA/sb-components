7a80e9b9673f911717640d1f703528c3acb58e40
Santiago Lopez Amaya <santiagoloppezamaya@hotmail.com>
2022-04-10 21:06:07 -0500

    :sparkles: storybook

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-
-ReactDOM.render(
-  <React.StrictMode>
-  
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+export { Button } from './stories/Button';

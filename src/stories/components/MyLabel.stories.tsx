174565ec168a30652c363c28f78c2108fc0828b0
Santiago Lopez Amaya <santiagoloppezamaya@hotmail.com>
2022-04-14 23:12:11 -0500

    :lipstick: controles

--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -8,7 +8,10 @@ import { MyLabel } from '../../components/MyLabel';
 export default {
   title: 'UI/MyLabel',
   component: MyLabel,
-
+  argTypes: {
+    size: {control: 'select'},
+    fontColor: {control: 'color'},
+  }
 } as ComponentMeta<typeof MyLabel>;
 
 
@@ -19,4 +22,38 @@ export const Basic = Template.bind({});
 Basic.args = {
   label: 'MyComponent',
   size : 'normal',
+  allCaps: false
+};
+
+export const AllCaps = Template.bind({});
+
+AllCaps.args = {
+  label: 'MyComponent',
+  size : 'normal',
+  allCaps: true,
+};
+
+export const Secondary = Template.bind({});
+
+Secondary.args = {
+  label: 'MyComponent',
+  size : 'normal',
+  color: 'secondary'
+};
+
+
+export const Tertiary = Template.bind({});
+
+Tertiary.args = {
+  label: 'MyComponent',
+  size : 'normal',
+  color: 'tertiary'
+};
+
+export const CustomFontColor = Template.bind({});
+
+CustomFontColor.args = {
+  label: 'MyComponent',
+  size : 'normal',
+  fontColor: '#5517ac',
 };

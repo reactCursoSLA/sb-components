9c102011ee4cebd17661301216cfd2eba0f9a687
Santiago Lopez Amaya <santiagoloppezamaya@hotmail.com>
2022-04-14 23:12:21 -0500

    :lipstick: controls

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -9,11 +9,33 @@ export interface MyLabelProps {
    * size
    */
   size?: "normal" | "h1" | "h2" | "h3";
+  /**
+   * sent to uppercase
+   */
+  allCaps?: boolean;
+  /**
+   * sent color
+   */
+  color?: "primary" | "secondary" | "tertiary";
+  /**
+   * sent color 
+   */
+  fontColor?: string;
 }
 
 /**
  * Label example
  */
-export const MyLabel = ({ label, size = "normal" }: MyLabelProps) => {
-  return <span className={`${size}`}>{label}</span>;
+export const MyLabel = ({
+  label,
+  size = "normal",
+  allCaps = false,
+  color = "primary",
+  fontColor,
+}: MyLabelProps) => {
+  return (
+    <span className={`label ${size} text-${color}`} style={{color:fontColor}}>
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
 };
